296ed2275983f0e55b9d042a15ea2cc55fd49860
AayushKhanal47 <aayushkhanal810@gmail.com>
2025-07-29 19:59:09 +0545

    feat: common

---
+++ common/src/index.ts
@@ -0,0 +1,33 @@
+import z from "zod";
+
+export const SignupInput = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+  name: z.string().optional(),
+});
+
+
+
+export const SigninInput = z.object({
+  username: z.string().email(),
+  password: z.string().min(6),
+});
+
+
+
+export const createBlogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+
+export const updateBlogInput = z.object({
+  title: z.string(),
+  content: z.string(),
+  id: z.number(),
+});
+
+export type SignupInput = z.infer<typeof SignupInput>;
+export type SigninInput = z.infer<typeof SigninInput>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
