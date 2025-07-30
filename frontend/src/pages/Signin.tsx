ca33306099157c64438644b0ce1e46afb3b18ff2
AayushKhanal47 <aayushkhanal810@gmail.com>
2025-07-30 18:39:30 +0545

    feat:Signin

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Quote from "../components/Quote";
+import { Auth } from "../components/Auth";
 
 const Signin = () => {
   return (
-    <div>Signin</div>
-  )
-}
+    <div>
+      <div className="grid grid-cols-2">
+        <div>
+          <Auth type="signin" />
+        </div>
+        <div className="invisible lg:visible">
+          <Quote />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Signin
+export default Signin;
